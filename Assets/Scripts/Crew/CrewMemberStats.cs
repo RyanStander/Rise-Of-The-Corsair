228db40bc5bf9567547e0911240d8410fe712b72
Ryan <stander.ryan@gmail.com>
2023-09-12 19:55:54 +0200

    crew generator setup

--- Assets/Scripts/Crew/CrewMemberStats.cs
+++ Assets/Scripts/Crew/CrewMemberStats.cs
@@ -27,14 +27,17 @@ namespace Crew
         public int Cooking { get; private set; }
         public float Morale { get; private set; }
         public int Level { get; private set; }
-        public int Experience { get; private set; }
+        public float Experience { get; private set; }
 
         private string nameFileName = "CrewMemberNames";
         private string nicknameFileName = "CrewMemberNicknames";
 
-        public CrewMemberStats GenerateCrewMemberStats(int minLevel)
+        public CrewMemberStats GenerateCrewMemberStats(int minLevel, CrewLevelData crewLevelData)
         {
             var crewMemberStats = new CrewMemberStats();
+
+            //TODO: This needs some form of random
+            crewMemberStats.Level = minLevel;
 
             //TODO: change this to faction based on location or ship obtained from
             crewMemberStats.Name = GetRandomName(Faction.Dutch);
@@ -42,11 +45,13 @@ namespace Crew
 
             crewMemberStats.Health = RandomHealth();
 
-            crewMemberStats.Rank = DetermineRank(Level);
+            crewMemberStats.Rank = DetermineRank(crewMemberStats.Level);
+
+            crewMemberStats.Experience = CrewLevelSystem.GetXpForCurrentLevel(crewLevelData, crewMemberStats.Level);
 
             //TODO: add traits and specialisation functionality
 
-            SetupAttributes();
+            crewMemberStats = SetupAttributes(crewMemberStats);
 
             return crewMemberStats;
         }
@@ -144,19 +149,19 @@ namespace Crew
         /// <summary>
         /// For each level, a crew member gains 1 point to all stats, and 2 points randomly distributed to any stat
         /// </summary>
-        private void SetupAttributes()
+        private CrewMemberStats SetupAttributes(CrewMemberStats crewMemberStats)
         {
             for (var i = 0; i < Level; i++)
             {
-                Strength++;
-                Agility++;
-                Marksmanship++;
-                Sailing++;
-                Repair++;
-                Medicine++;
-                Leadership++;
-                Navigation++;
-                Cooking++;
+                crewMemberStats.Strength++;
+                crewMemberStats.Agility++;
+                crewMemberStats.Marksmanship++;
+                crewMemberStats.Sailing++;
+                crewMemberStats.Repair++;
+                crewMemberStats.Medicine++;
+                crewMemberStats.Leadership++;
+                crewMemberStats.Navigation++;
+                crewMemberStats.Cooking++;
 
                 for (var j = 0; j < 2; j++)
                 {
@@ -166,35 +171,37 @@ namespace Crew
                     switch (randomStat)
                     {
                         case 0:
-                            Strength++;
+                            crewMemberStats.Strength++;
                             break;
                         case 1:
-                            Agility++;
+                            crewMemberStats.Agility++;
                             break;
                         case 2:
-                            Marksmanship++;
+                            crewMemberStats.Marksmanship++;
                             break;
                         case 3:
-                            Sailing++;
+                            crewMemberStats.Sailing++;
                             break;
                         case 4:
-                            Repair++;
+                            crewMemberStats.Repair++;
                             break;
                         case 5:
-                            Medicine++;
+                            crewMemberStats.Medicine++;
                             break;
                         case 6:
-                            Leadership++;
+                            crewMemberStats.Leadership++;
                             break;
                         case 7:
-                            Navigation++;
+                            crewMemberStats.Navigation++;
                             break;
                         case 8:
-                            Cooking++;
+                            crewMemberStats.Cooking++;
                             break;
                     }
                 }
             }
+
+            return crewMemberStats;
         }
     }
 }
